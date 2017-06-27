6737a8004d68003694ae043be2e54242c93f3ce5
Federico Tacchini <tacchinifederico@gmail.com>
2017-06-27 16:51:52 -0300

    asdasd

--- src/default-configuration/default-injector-configurator.core.ts
+++
@@ -1,6 +0,0 @@
-import { Container } from "inversify";
-
-export class DefaultInjectorConfigurator {
-    configure(container: Container) {
-    };
-}
--- src/metadata-core/metadata-builder/controller-metadata-keys.ts
+++ src/metadata-core/metadata-builder/controller-metadata-keys.ts
@@ -1,1 +1,3 @@
 export const CONTROLLER_BUILDER = Symbol("ControllerBuilder"); 
+export const ROUTE_BUILDER = Symbol("RouteBuilder");  
+export const MIDDLEWARE_BUILDER = Symbol("MiddlewareBuilder");
