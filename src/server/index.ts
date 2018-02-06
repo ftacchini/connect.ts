dad9d408b9431607d5882573e563b7ba50c0196d
Federico <tacchinifederico@gmail.com>
2018-02-06 16:05:40 +0100

    Exporting ts module

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,5 +5,6 @@ export * from "./server";
 export * from "./hub-builder";
 export * from "./hub";
 export * from "./ts-framework";
+export * from "./ts-module";
 export * from "./server-support";
 export * from "./routed-ts-framework";
